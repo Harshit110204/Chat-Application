5a0fdeaefb6c8b9daebd2fe64d279d497d324e55
Harshit Kumar Mishra <harshitmisha177@gmail.com>
2025-11-20 12:29:17 +0530

    copyright

--- Frontend/src/components/Footer.js
+++ Frontend/src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <>
       <footer style={{position:"sticky", top:"100%", backgroundColor: "#212529", color: "#6c757d", padding: "1rem", textAlign: "center"}}>
         <div style={{maxWidth: "800px", margin: "0 auto", backgroundColor: "#212529"}}>
-          <span> Copyright &copy; www.youchat.com | Ankit Pandey, AIT Pune-21135 </span>
+          <span> Copyright &copy; www.youchat.com | Harshit Kumar Mishra , GLBITM-2201921520067 </span>
         </div>
       </footer>
     </>
